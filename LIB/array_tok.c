54cefa1f4354c378169490c5fa97cd8a20b94ab1
Noah Steiniger <noah.steiniger@epitech.eu>
2023-11-21 14:24:25 +0100

    refactor (array_tok.c) : changes to gain some spaces and make it my way

--- LIB/array_tok.c
+++ LIB/array_tok.c
@@ -9,19 +9,18 @@
 
 int my_toklen(char *str, char *sep)
 {
-    int ligns = 0;
-    for (int i = 0; str[i]; i++) {
-        for (int x = 0; sep[x]; x++) {
-            ligns += (str[i] == sep[x]);
-        }
-    } return ++ligns * 8;
+    int ligns = 0, x, i = 0;
+    for (; str[i]; i++)
+        for (x = 0; sep[x]; x++, ligns += (str[i] == sep[x]));
+    return (++ligns) * PTR;
 }
 
 char **array_tok(char *str, char *sep)
 {
     char **array = malloc(my_toklen(str, sep)), *word = strtok(str, sep);
-    for (int i = 0; word; i++) {
-        array[i] = word;
-        word = strtok(NULL, sep);
-    } return array;
+
+    do {
+        *(array++) = word;
+    } while ((word = strtok(NULL, sep)) != NULL);
+    return array;
 }
