ec38faa9725b128687f15c63b0d3f31a0e3a668b
Noah Steiniger <noah.steiniger@epitech.eu>
2023-11-21 15:30:18 +0100

    style (fileReaders) : coding-style proof indentations

--- LIB/READFILES/stat_reader.c
+++ LIB/READFILES/stat_reader.c
@@ -13,8 +13,9 @@ char **stat_reader(char *name)
     int fd = open(name, O_RDONLY), x = 0;
     char *buffer = NULL;
     char **arr = NULL;
+    
     if (!stat(name, &st) || st.st_size == 0 || fd == -1)
-        exitError("Error: File is empty or inexistent\n");
+        exitErr("Error: File is empty or inexistent\n");
     read(fd, buffer, st.st_size);
     buffer[st.st_size] = '\0';
     close(fd);
